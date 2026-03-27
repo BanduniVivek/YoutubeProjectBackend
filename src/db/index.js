4e3808dad0e32b9d3e9741e7167d29228247f4b8
Vivek Banduni <Vivekbanduni3@gmail.com>
2026-03-27 23:49:06 +0530

    Connect database

---
+++ src/db/index.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+const connectDB = async() =>{
+    try{
+        const connectionInstance = await mongoose.connect(`${process.env.MONOGODB_URI}/${DB_NAME}`)
+        console.log("MONGO DB connected!! ")
+    }
+    catch(error){
+        console.log("MONGO DB connection error : ",error)
+        process.exit(1)
+    }
+}
+
+export default connectDB
