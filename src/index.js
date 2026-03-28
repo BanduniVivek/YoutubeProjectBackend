dadb8dcbacccbde6720061fe718067aa2441ea65
Vivek Banduni <Vivekbanduni3@gmail.com>
2026-03-28 17:04:41 +0530

    User controller and bug fixes

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import dotenv from 'dotenv'
 import connectDB from "./db/index.js";
+import { app } from './app.js';
 
 dotenv.config({
         path : "./.env"
@@ -9,6 +10,20 @@ dotenv.config({
 
 
 connectDB()
+.then(()=>{
+    const server = app.listen(process.env.PORT||1000, ()=>{
+        console.log(`Server is listening at ${process.env.PORT}`)
+    })
+
+    server.on("error", (err)=>{
+        console.log("Server error:", err);
+    });
+})
+.catch((err)=>{
+    console.log("MONGO DB Connection failed")
+})
+
+
 
 
 
